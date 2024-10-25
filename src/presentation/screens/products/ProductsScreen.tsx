c40f89f5ad02fba7d1d8ecbea4c521515f647b15
ant <antorrealbatapia@gamil.com>
2024-10-24 22:09:36 -0400

    listando productos

--- src/presentation/screens/products/ProductsScreen.tsx
+++ src/presentation/screens/products/ProductsScreen.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { globalStyles } from '../../theme/theme';
+import {Text, View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {FlatList} from 'react-native-gesture-handler';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+
+const products = [
+  {id: 1, name: 'Producto 1'},
+  {id: 2, name: 'Producto 2'},
+  {id: 3, name: 'Producto 3'},
+  {id: 4, name: 'Producto 4'},
+  {id: 5, name: 'Producto 5'},
+  {id: 6, name: 'Producto 6'},
+  {id: 7, name: 'Producto 7'},
+  {id: 8, name: 'Producto 8'},
+];
 
 export const ProductsScreen = () => {
   console.log('ProductsScreen rendered');
   return (
     <View style={globalStyles.container}>
-      <Text style={{color: 'black'}}>Pantalla de Productos</Text>
+      <Text style={{color: 'black'}}>Productos</Text>
+
+      <FlatList
+        data={products}
+        renderItem={({item}) => (
+          <PrimaryButton onPress={() => {}} label={item.name} />
+        )}
+      />
+
+      <Text style={{marginBottom: 10, fontSize:30, color:'black'}}>
+
+        Ajustes
+      </Text>
     </View>
   );
 };
